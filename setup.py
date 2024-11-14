0e1ec3390f00039326d69ed49755e9a9539c279f
sdcampbell <sdcampbell68@live.com>
2024-11-14 06:47:00 -0500

    Commit on setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="xfil",
+    version="0.1.0",
+    py_modules=["xfil"],
+    install_requires=[
+        "requests>=2.32.3",
+    ],
+    python_requires=">=3.11",
+    entry_points={
+        "console_scripts": [
+            "xfil=xfil:main",
+        ],
+    },
+    author="Steve Campbell",
+    author_email="sdcampbell68@live.com",
+    description="xfil is a tool that performs blind XPath exploitation and data exfiltration. This tool is created for penetration testers performing authorized security assessments.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sdcampbell/xfil",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache-2.0 license",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
